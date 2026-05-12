5690c0a78277f4bb017d42897f8978159c5909a6
dexon401 <255047233+dexon401@users.noreply.github.com>
2026-05-12 21:46:10 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,67 @@
+import logging
+
+from geo import get_country, get_distance, get_coordinates
+
+logging.basicConfig(level=logging.INFO, filename='app.log', format='%(asctime)s %(levelname)s %(name)s %(message)s')
+
+def handler(event, context):
+
+    logging.info('Request: %r', event)
+
+    response = {
+        'session': event['session'],
+        'version': event['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+
+    handle_dialog(response, event)
+
+    logging.info('Request: %r', response)
+
+    return response
+
+
+def handle_dialog(res, req):
+
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+
+        res['response']['text'] = 'Привет! Я могу сказать в какой стране город или сказать расстояние между городами!'
+
+        return
+
+    cities = get_cities(req)
+
+    if len(cities) == 0:
+
+        res['response']['text'] = 'Ты не написал название не одного города!'
+
+    elif len(cities) == 1:
+
+        res['response']['text'] = 'Этот город в стране - ' + get_country(cities[0])
+
+    elif len(cities) == 2:
+
+        distance = get_distance(get_coordinates(cities[0]), get_coordinates(cities[1]))
+        res['response']['text'] = 'Расстояние между этими городами: ' + str(round(distance)) + ' км.'
+
+    else:
+
+        res['response']['text'] = 'Слишком много городов!'
+
+
+def get_cities(req):
+
+    cities = []
+
+    for entity in req['request']['nlu']['entities']:
+
+        if entity['type'] == 'YANDEX.GEO':
+
+            if 'city' in entity['value'].keys():
+                cities.append(entity['value']['city'])
+
+    return cities
